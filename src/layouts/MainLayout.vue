8130d4b00eba756353d9d5b14ba27bc66353a6d0
Nekrasov-A-A <a.alex.nekrasov@gmail.com>
2021-05-21 22:23:11 +0300

    added normalize.css and added header in MainLayout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,11 +1,35 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      something
+    <v-app-bar
+      color="cyan lighten-4"
+      app
+      prominent
+      shrink-on-scroll
+      src="https://img.brandshop.ru/cache/products/m/main-air-jordan-1-retro-high-og-bloodline-pryzhok-v-nebo-z_1128x634.jpg"
+      fade-img-on-scroll
+      scroll-target
+      scroll-threshold="300"
+      height="150"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(0,255,222,0.45451684091605393), rgba(255,0,10,0.8326681014202556)"
+          position="center top 65%"
+        ></v-img>
+      </template>
+      <v-app-bar-title>SHOP</v-app-bar-title>
       <v-spacer></v-spacer>
-      <v-btn @click="logoutAndChangeRoute">Logout</v-btn>
+      <v-toolbar-items style="height: 100%">
+        <v-btn text> Home </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn text> Catalog </v-btn>
+        <v-divider vertical></v-divider>
+        <v-btn @click="logoutAndChangeRoute" text>Logout</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
-    <v-main>
+
+    <v-main app>
       <router-view />
     </v-main>
   </v-app>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,60 @@
 <template>
-  <div>
-    <v-chip :to="'/signup'"> GO to signUpForm</v-chip>
-    <v-chip :to="'/signin'"> GO to signInForm</v-chip>
-  </div>
+  <v-main>
+    <div class="test">
+      <v-chip :to="'/signup'"> GO to signUpForm</v-chip>
+      <v-chip :to="'/signin'"> GO to signInForm</v-chip>
+    </div>
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque velit ab
+      temporibus. Possimus nobis modi saepe corporis magni dolorem aut,
+      reprehenderit, harum eveniet rem cumque at cum fugiat nisi perspiciatis.
+      Unde pariatur, tenetur itaque deserunt odio laborum nesciunt quis! Totam
+      obcaecati debitis eaque odio pariatur sequi recusandae, earum ullam at
+      illum! Sit, quisquam ad vero porro ipsa possimus optio voluptatem.
+      Consequatur ad ullam dolorem, odit atque deleniti accusantium aperiam,
+      accusamus dolore blanditiis magni, ex assumenda? Velit delectus similique
+      neque beatae quaerat consequuntur cum vel ipsam quidem, ullam architecto
+      quod tempore? Perspiciatis voluptate accusantium in odio, dolore at!
+      Molestiae accusamus perferendis esse saepe, maxime, quod, reiciendis quis
+      aperiam veniam ipsa quia modi autem commodi sequi a deserunt voluptas
+      quaerat expedita amet. Nobis, excepturi numquam ab labore in optio saepe
+      obcaecati? Dolores debitis nulla minima error veniam assumenda, asperiores
+      temporibus eum, molestias atque in, nam amet tempora reiciendis? Inventore
+      neque aut facilis? Aspernatur aliquam tempora magni ipsa incidunt
+      laboriosam expedita, sequi tenetur quae. Quaerat veritatis facere soluta
+      maxime aperiam illum hic possimus, exercitationem voluptates dolor beatae
+      tempore corrupti est, cum blanditiis esse. Sed est aliquam, a aut
+      repudiandae nobis aperiam iste nam amet quae fugit eius esse? Commodi illo
+      mollitia sit culpa odit perferendis reiciendis nihil reprehenderit
+      excepturi enim. Voluptates, dolorum qui. Voluptatum nobis alias inventore
+      dignissimos laboriosam? Voluptas iusto quaerat sint, voluptatum optio
+      harum illo alias ipsum velit. Necessitatibus maiores similique quam odio,
+      voluptate deleniti pariatur officia, culpa aspernatur ipsam ipsa? Cumque
+      illo voluptatum eaque debitis delectus totam porro iure corrupti,
+      blanditiis excepturi fuga placeat ipsa aliquam maiores, amet ratione. Qui
+      consequuntur cupiditate itaque sint obcaecati? Aperiam delectus placeat
+      unde? Necessitatibus. Quis iste minus recusandae inventore tenetur,
+      repudiandae ipsum nesciunt deserunt adipisci dolor quam ab aliquam rerum
+      eveniet voluptatem incidunt nostrum sapiente quasi. Facilis dolore quae
+      amet pariatur, optio ea laudantium! Maiores eum accusantium, vero
+      aspernatur veritatis in. Illo molestias iusto doloremque sunt aliquid!
+      Molestiae deleniti maxime perspiciatis dignissimos voluptatibus beatae
+      natus id numquam minima soluta, nisi exercitationem eius est suscipit?
+      Culpa magnam, doloribus nesciunt iure quod velit eos. Nobis ipsa ea natus
+      porro placeat, illum, vero odio laborum possimus ex a. Nobis ab atque sed
+      dignissimos, veniam quisquam voluptatibus quis? Nobis quis facilis
+      provident dolore error voluptatibus earum, natus odio ab, assumenda omnis
+      porro totam blanditiis sunt esse consequuntur eum harum, amet mollitia
+      voluptatem quasi accusamus ullam. Placeat, delectus suscipit! Iure, atque
+      corporis. Sit quo eum necessitatibus, voluptas veniam sequi fugit tenetur
+      expedita soluta iure quia vero dignissimos labore, distinctio aut?
+      Eligendi voluptas odit impedit accusamus assumenda dolore. Temporibus,
+      aspernatur! Illum perspiciatis modi voluptates nesciunt ipsum fugit
+      sapiente ratione officia maiores. Qui sunt deleniti laboriosam pariatur
+      ea, veniam blanditiis, modi nisi itaque possimus culpa sint illum mollitia
+      quasi! Exercitationem, harum.
+    </div>
+  </v-main>
 </template>
 
 <script>
@@ -10,3 +62,9 @@ export default {
   name: "Home",
 };
 </script>
+
+<style scoped>
+.test {
+  height: 200vh !important;
+}
+</style>
